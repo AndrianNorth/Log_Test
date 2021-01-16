a9463ed89f85083d206f56adeb8421be91e11d0a
AndrianNorth <disturbed7112@gmail.com>
2021-01-16 17:18:56 +0700

    hw test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,32 @@
-//
-//public class Main {
-//
-//    public static int[] cutAfterFour(int[] arr) {
-//        for (int i = arr.length - 1; i >= 0; i--) {
-//            if (arr[i] == 4) {
-//                int[] newArray = new int[arr.length - 1 - i];
-//                int k = 0;
-//                for (int j = i + 1; j < arr.length; j++) {
-//                    newArray[k] = arr[j];
-//                    k++;
-//                }
-//                return newArray;
-//            }
-//        }
-//        throw new RuntimeException("4 not found");
-//    }
-//
-//    public static boolean oneOrFour(int[] arr) {
-//        boolean one = false;
-//        boolean four = false;
-//        for (int j : arr) {
-//            if (j == 1) {
-//                one = true;
-//            }
-//            if (j == 4) {
-//                four = true;
-//            }
-//        }
-//        return one && four;
-//    }
-//}
+
+public class Main {
+
+    public static int[] cutAfterFour(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 4) {
+                int[] newArray = new int[arr.length - 1 - i];
+                int k = 0;
+                for (int j = i + 1; j < arr.length; j++) {
+                    newArray[k] = arr[j];
+                    k++;
+                }
+                return newArray;
+            }
+        }
+        throw new RuntimeException("4 not found");
+    }
+
+    public static boolean oneOrFour(int[] arr) {
+        boolean one = false;
+        boolean four = false;
+        for (int j : arr) {
+            if (j == 1) {
+                one = true;
+            }
+            if (j == 4) {
+                four = true;
+            }
+        }
+        return one && four;
+    }
+}
